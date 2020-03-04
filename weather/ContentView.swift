d80f6cb2f3956a56dd51e183dc5a9d3fc847a456
Ying-Ting Lee <cyc00201@gmail.com>
2020-03-04 13:22:43 +0800

    Master Create

---
+++ weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weather
+//
+//  Created by 李映廷 on 2020/3/4.
+//  Copyright © 2020 李映廷. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
